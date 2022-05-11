0eae0723aaf62b338d612b6ff21b5100fb5892a4
Bartek-o <bartek.ocypa@gmail.com>
2022-05-11 20:34:52 +0200

    Init calculator

---
+++ Calculator.aspx.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Clear(object sender, EventArgs e)
+        {
+            result.Value = "0";
+            hiddenNumber.Value = "";
+            hiddenOperator.Value = "";
+            HiddenFlag.Value = "0";
+        }
+
+        protected void ClearResultInput(object sender, EventArgs e)
+        {
+            result.Value = "0";
+        }
+
+        protected void Backspace(object sender, EventArgs e)
+        {
+            if (result.Value.Length != 1)
+            {
+                result.Value = result.Value.Substring(0, result.Value.Length - 1);
+            }
+            else
+            {
+                result.Value = "0";
+            }
+        }
+
+        protected void Number(object sender, EventArgs e)
+        {
+            if (HiddenFlag.Value == "1")
+            {
+                result.Value = "0";
+                HiddenFlag.Value = "11";
+            }
+
+            if (result.Value == "0" && (sender as Button).Text != ",")
+            {
+                result.Value = (sender as Button).Text;
+            }
+            else
+            {
+                if ((sender as Button).Text == "," && result.Value.Contains(","))
+                {
+                    return;
+                }
+                else
+                {
+                    result.Value += (sender as Button).Text;
+                }
+            }
+        }
+
+        protected void Operator(object sender, EventArgs e)
+        {
+            if (result.Value[result.Value.Length - 1].Equals(','))
+            {
+                result.Value += "0";
+            }
+
+            if (HiddenFlag.Value == "11")
+            {
+                Calculate(sender, e);
+            }
+
+            hiddenOperator.Value = (sender as Button).Text;
+
+            if ((sender as Button).Text == "√")
+            {
+                Calculate(sender, e);
+                return;
+            }
+
+            hiddenNumber.Value = result.Value;
+            HiddenFlag.Value = "1";
+        }
+
+        protected void Calculate(object sender, EventArgs e)
+        {
+            if (result.Value[result.Value.Length-1].Equals(','))
+            {
+                result.Value += "0";
+            }
+            string tmp = "";
+            switch (hiddenOperator.Value)
+            {
+                case "+":
+                    tmp = (Convert.ToDouble(hiddenNumber.Value) + Convert.ToDouble(result.Value)).ToString();
+                    Clear(sender, e);
+                    result.Value = tmp;
+                    break;
+                case "-":
+                    tmp = (Convert.ToDouble(hiddenNumber.Value) - Convert.ToDouble(result.Value)).ToString();
+                    Clear(sender, e);
+                    result.Value = tmp;
+                    break;
+                case "*":
+                    tmp = (Convert.ToDouble(hiddenNumber.Value) * Convert.ToDouble(result.Value)).ToString();
+                    Clear(sender, e);
+                    result.Value = tmp;
+                    break;
+                case "/":
+                    if (Convert.ToDouble(result.Value) == 0)
+                    {
+                        ScriptManager.RegisterClientScriptBlock(Page, typeof(Page), "ClientScript", "alert('Division by zero!')", true);
+                        Clear(sender, e);
+                        break;
+                    }
+                    tmp = (Convert.ToDouble(hiddenNumber.Value) / Convert.ToDouble(result.Value)).ToString();
+                    Clear(sender, e);
+                    result.Value = tmp;
+                    break;
+                case "√":
+                    tmp = Math.Sqrt(Convert.ToDouble(result.Value)).ToString();
+                    Clear(sender, e);
+                    result.Value = tmp;
+                    break;
+            }
+        }
+    }
+}
---
+++ Calculator.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generowane automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Zmiany w tym pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowane automatycznie>
+//------------------------------------------------------------------------------
+
+namespace Calculator
+{
+
+
+    public partial class Calculator
+    {
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Kontrolka result.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText result;
+
+        /// <summary>
+        /// Kontrolka hiddenNumber.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hiddenNumber;
+
+        /// <summary>
+        /// Kontrolka hiddenOperator.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hiddenOperator;
+
+        /// <summary>
+        /// Kontrolka HiddenFlag.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HiddenFlag;
+    }
+}
